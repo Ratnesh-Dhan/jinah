34f8edc9c42b31e703fd84bd2d4d10a04563a142
Ratnesh-Dhan <ratneshdhan@gmail.com>
2026-08-18 18:09:29 +0530

    update

--- funnytest.ts
+++ funnytest.ts
@@ -1,4 +1,17 @@
 import fs from "node:fs/promises";
 
-const data = await fs.readFile("./test.json");
-console.log(JSON.parse(data));
+const file = await fs.readFile("./test.json", "utf-8");
+const data = JSON.parse(file);
+
+// console.log(data.result);
+
+for (const result of data.result) {
+  const messageText = result.message?.text;
+  if (
+    typeof messageText === "string" &&
+    messageText.toLowerCase() === "/start"
+  ) {
+    console.log(result.message.text);
+    console.log(result.message.chat);
+  }
+}
